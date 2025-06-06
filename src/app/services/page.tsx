35874caf73e53d3e66093d316bf41a7fad78cfe4
Andrés Uribe <aeua2000@gmail.com>
2025-06-06 12:36:25 -0400

    feat: add Services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/header/Header'
+
+const page = () => {
+  return (
+    <div className='bg-[#19165F] min-h-screen'>
+      <Header/>
+    </div>
+  )
+}
+
+export default page
