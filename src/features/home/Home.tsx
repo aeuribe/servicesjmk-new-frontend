a7fa697242e38ccd350e18bedd120a4747789897
Andrés Uribe <aeua2000@gmail.com>
2025-06-09 15:46:04 -0400

    feat: add Service Section to Home Page

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Hero from "./components/Hero";
+import ServicesSection from "./components/ServicesSection";
 
 const Home = () => {
   return (
-    <div
-      className="relative bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/background-hero.jpg')" }}
-    >
-      <Header title="Header" />
-      <Hero/>
+    <div>
+      {/* Sección con fondo */}
+      <div
+        className="relative bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/background-hero.jpg')" }}
+      >
+        <Header title="Header" />
+        <Hero />
+      </div>
+
+      {/* Sección sin fondo */}
+      <ServicesSection />
     </div>
   );
 };
